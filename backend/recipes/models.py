15f7375a85b00e2abcfc6bb9694d75c2f9ff51a5
Anzor Kvachantiradze <anzor.green@gmail.com>
2025-03-17 13:37:44 +0100

    Deploy on server

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -7,81 +7,81 @@ from users.models import BaseModel
 from utils.constants import MAX_LENGTH_SLUG, MAX_LENGTH_TITLE
 
 
-class Recipe(BaseModel):
-    """Модель рецепта."""
+# class Recipe(BaseModel):
+#     """Модель рецепта."""
 
-    name = models.CharField(
-        max_length=MAX_LENGTH_TITLE,
-        blank=False,
-        null=False,
-        verbose_name='Название'
-    )
-    text = models.TextField(
-        blank=False,
-        null=False,
-        verbose_name='Описание'
-    )
-    image = models.ImageField(
-        default=None,
-        null=True,
-        verbose_name='Изображение',
-        upload_to='recipes/images/'
-    )
-    cooking_time = models.PositiveSmallIntegerField(
-        blank=False,
-        null=False,
-        verbose_name='Время приготовления'
-    )
-    author = models.ForeignKey(
-        'users.User',
-        null=False,
-        on_delete=models.CASCADE,
-        related_name='recipes',
-        verbose_name='Автор'
-    )
-    tags = models.ManyToManyField(
-        'recipes.Tag',
-        blank=False,
-        related_name='recipes',
-        verbose_name='Тег'
-    )
-    short_url = models.UUIDField(
-        default=uuid.uuid4,
-        editable=False,
-        unique=True
-    )
+#     name = models.CharField(
+#         max_length=MAX_LENGTH_TITLE,
+#         blank=False,
+#         null=False,
+#         verbose_name='Название'
+#     )
+#     text = models.TextField(
+#         blank=False,
+#         null=False,
+#         verbose_name='Описание'
+#     )
+#     image = models.ImageField(
+#         default=None,
+#         null=True,
+#         verbose_name='Изображение',
+#         upload_to='recipes/images/'
+#     )
+#     cooking_time = models.PositiveSmallIntegerField(
+#         blank=False,
+#         null=False,
+#         verbose_name='Время приготовления'
+#     )
+#     author = models.ForeignKey(
+#         'users.User',
+#         null=False,
+#         on_delete=models.CASCADE,
+#         related_name='recipes',
+#         verbose_name='Автор'
+#     )
+#     tags = models.ManyToManyField(
+#         'recipes.Tag',
+#         blank=False,
+#         related_name='recipes',
+#         verbose_name='Тег'
+#     )
+#     short_url = models.UUIDField(
+#         default=uuid.uuid4,
+#         editable=False,
+#         unique=True
+#     )
 
-    class Meta:
-        ordering = ['-created_at']
-        verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецепты'
-        constraints = [
-            models.UniqueConstraint(
-                fields=['name', 'author'],
-                name='unique_name_author'
-            )
-        ]
+#     class Meta:
+#         ordering = ['-created_at']
+#         verbose_name = 'Рецепт'
+#         verbose_name_plural = 'Рецепты'
+#         constraints = [
+#             models.UniqueConstraint(
+#                 fields=['name', 'author'],
+#                 name='unique_name_author'
+#             )
+#         ]
 
-    def generate_short_url(self):
-        """Генерация короткого URL на основе домена."""
-        domain = settings.SITE_DOMAIN
-        return f'{domain}/{self.short_url}'
+#     def generate_short_url(self):
+#         """Генерация короткого URL на основе домена."""
+#         domain = settings.SITE_DOMAIN
+#         return f'{domain}/{self.short_url}'
 
-    def __str__(self):
-        return f'{self.name} (Автор: {self.author})'
+#     def __str__(self):
+#         return f'{self.name} (Автор: {self.author})'
 
-    def short_description(self):
-        """Получение короткого описания рецепта."""
-        return ((self.text[:25] + '...') if self.text
-                else 'Описание отсутствует')
+#     def short_description(self):
+#         """Получение короткого описания рецепта."""
+#         return ((self.text[:25] + '...') if self.text
+#                 else 'Описание отсутствует')
 
-    def clean(self):
-        """Проверка размера изображения перед сохранением."""
-        if self.image and self.image.size > 5 * 1024 * 1024:
-            raise ValidationError(
-                'Размер изображения не должен превышать 5 МБ'
-            )
-        super().clean()
+#     def clean(self):
+#         """Проверка размера изображения перед сохранением."""
+#         if self.image and self.image.size > 5 * 1024 * 1024:
+#             raise ValidationError(
+#                 'Размер изображения не должен превышать 5 МБ'
+#             )
+#         super().clean()
 
 
 class Tag(BaseModel):
